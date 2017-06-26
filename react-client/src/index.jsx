828c69ae151624eeaa0ce321b16e7f67c14c0b26
lichinshao <lichinshao@gmail.com>
2017-06-26 12:44:48 -0700

    added initial get request to eventful to use for slider on homepage

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,14 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 
-import Search from './components/Search.jsx'
+import Search from './components/Search.jsx';
+import Mount from './components/Mount.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      eventsBar: [],
+      loaded: false
     }
+  }
+
+  componentDidMount() {
+    const appKey = 'app_key=CwcF9Lt3qkKh4gWB';
+
+    $.ajax({
+      method: "GET",
+      url: 'http://api.eventful.com/json/events/search?' + appKey + '&l=san+francisco&date=future',
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      dataType: 'jsonp',
+      success: (data) => {
+        this.setState({eventsBar: data.events.event, loaded: true});
+
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    });
   }
 
   getEventful() {
@@ -37,6 +59,7 @@ class App extends React.Component {
       <div>
         <h5>Event Planner</h5>
         <Search eventful = {this.getEventful.bind(this)} meetUp = {this.getMeetup.bind(this)}/>
+        {this.state.loaded ? <Mount events={this.state.eventsBar}/> : null}
       </div>
     );
   }
