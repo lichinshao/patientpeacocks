38a8ae0be7a8fca129a832f27cc1e9b387f92043
Jerizon <JesseLowellReyes@gmail.com>
2017-06-27 17:13:11 -0700

    Return API data to server, waiting on schema format

--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
-var models = require('./models');
-
+var rp = require('request-promise');
 
 app.use(bodyParser.json());
 
@@ -13,34 +12,48 @@ app.post('/eventful', function (req, res) {
   // var data = JSON.parse(req.body);
   console.log(req.body);
   var loc = '&l=' + req.body.location.split(' ').join('+');
+  var topic = req.body.topic; 
+  var eventfulOptions = { method: 'GET',
+  url: 'http://api.eventful.com/json/events/search',
+  qs: { app_key: 'CwcF9Lt3qkKh4gWB', l: 'san francisco' },
+  headers: 
+   { l: 'san%20francisco',
+     q: topic,
+     date: 'future' } };
 
-  var topic = req.body.topic;
-  var app_key = 'app_key=CwcF9Lt3qkKh4gWB';
-  var options = {
-    method: 'GET',
-    url: 'http://api.eventful.com/json/events/search?' + app_key + loc + '&date=future&categories=' + topic ,
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    }
-  };
-  request(options, function(err, reponse, body) {
-    if (err) {
-      console.log(err);
-    } else {
-      var data = JSON.parse(body);
-      var data = data.events.event;
-      res.send(data);
-    }
-  })
 
-});
-
-app.get('/eventful', function (req, res) {
-  models.users.get()
-  .then((result)=>{
-    console.log(result);
-    res.send(result);
-  })
+     console.log('Going to promises');
+    
+    
+    rp(eventfulOptions).then(function (data) {
+      console.log('In the promises');
+      return data;
+    }).then(eventfulData => {
+        var sumData = JSON.stringify('[' + eventfulData + ', ');
+        var meetupCategories = 
+        {
+          // meetup searches catagories by numbers 
+          // it is weird but functional
+          music: 21,
+          food: 10,
+          arts: 1,
+          books: 18,
+          animals: 26,
+        }
+        var meetupOptions = {
+        method: 'GET',
+        url: 'https://api.meetup.com//find/groups',
+        qs: 
+       { sign: 'true',
+         key: '2771396637a6981749467c7663e19',
+         category: meetupCategories[topic] }
+        }
+        
+        rp(meetupOptions).then(function (data) {
+          sumData += JSON.stringify(data);
+          res.send(sumData);
+        })
+      })
 });
 
 app.get('/meetup', function(req, res) {
@@ -56,7 +69,7 @@ app.listen(3000, function () {
 
 /*
 var loc = '&l=' + req.query.location.split(' ').join('+');
-  var topic = '&q=' + req.query.topic;
+  var topic = '&q=' + req.query.topic; 
   var options = {
     app_key: 'app_key=CwcF9Lt3qkKh4gWB',
     url: 'http://api.eventful.com/json/events/search?' + this.app_key + topic + loc + '&date=future',
@@ -66,5 +79,5 @@ var loc = '&l=' + req.query.location.split(' ').join('+');
   };
   request(options, function(err, reponse, body) {
     console.log('in request', body);
-  })
-*/
+  }) */ 
+
