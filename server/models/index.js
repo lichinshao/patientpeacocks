172c2b549aec499fe517a33963c99b06d586c1fa
Julia Wong <juliawong05@gmail.com>
2017-06-28 16:55:23 -0700

    progress on inner join

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,10 @@
 const db = require('../database');
 
 module.exports.users = {
-  // test
-  // models.events.post(body)
-  // .then( (results) => {
-
-  // })
+  //testFunc
+  getUsers: () => {
+    return db.query(`SELECT * from users`);
+  },
   // create a user
   createUser: (username, salt, hashedpass) => {
     // if user does not exists in users table
@@ -14,7 +13,6 @@ module.exports.users = {
   },
 
   saveEvent: (username, event) => {
-    //`SELECT id from users where name = '${username}'`
     return db.query(`INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('${event.name}', '${event.dateAndTime}', '${event.category}', '${event.url}', '${event.description}', '${event.location}')`)
     .then( () => {
       return db.query(`INSERT INTO users_events (userId, eventId) VALUES ( (SELECT id from users WHERE name = '${username}') , (SELECT id from events WHERE name = '${event.name}') )`)
@@ -27,31 +25,10 @@ module.exports.events = {
   getUsersEvents: (username) => {
     return db.query(`SELECT eventId from users_events WHERE userId = ( SELECT id from users where name = '${username}' )`);
   }
-  // create an event for a user id based on username
-  // subquery
+
  }
 
 
-// module.exports = {
-//   events: {
-//     // fetch all events
-//     get: function () {
-//     //
-//     },
-//     //
-//     post: function () {
-
-//     }
-//   },
-//   users: {
-//     get: function () {
-//       //
-//     },
-//       //
-//     post: function () {
-
-//     }
-//   }
 
 
 
