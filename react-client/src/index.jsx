5a58977f3a69155bab01d750a0c179f3eb8b56fb
lichinshao <lichinshao@gmail.com>
2017-06-29 16:55:39 -0700

    added a component for user if they go to a page that doesn't exist

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
 import Home from './components/homepage.jsx';
 import Login from './components/login.jsx';
 import signUp from './components/signUp.jsx';
 import userPage from './components/userPage.jsx';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import NotFound from './components/notFound.jsx';
 
 class App extends Component {
   render() {
@@ -14,9 +15,10 @@ class App extends Component {
           <Route path='/login' component={Login} />
           <Route path = '/signup' component = {signUp}/>
           <Route path ='/userpage' component = {userPage}/>
+          <Route path='*' component={notFound}/>
         </Router>
     )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
