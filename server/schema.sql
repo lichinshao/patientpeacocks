dc8cbceae15badec1bc288c5f9e3d8825e489f9a
Julia Wong <juliawong05@gmail.com>
2017-07-02 16:59:23 -0700

    schema edit for heroku

--- server/schema.sql
+++ server/schema.sql
@@ -1,4 +1,9 @@
-DROP DATABASE IF EXISTS events_app;
+-- DROP DATABASE IF EXISTS events_app;
+
+/* connect schema to heroku
+heroku pg:psql postgresql-transparent-46296 --app eventwire < server/schema.sql;
+
+drop schema events_app cascade;*/
 
 /* Use this one to connect
 psql -U postgres < server/schema.sql
@@ -7,60 +12,62 @@ psql dbname username
 psql -U userName dbName < server/schema.sql
 
 psql postgres -d events_app -f server/schema.sql*/
-CREATE DATABASE events_app;
-\c events_app;
+-- CREATE DATABASE events_app;
+-- \c events_app;
+DROP SCHEMA if exists event_app CASCADE;
+CREATE SCHEMA events_app AUTHORIZATION qbzvgvuvdevbhj;
 
 DROP TABLE if exists users;
 DROP TABLE if exists events;
 DROP TABLE if exists users_events;
 
-CREATE TABLE users (
+CREATE TABLE events_app.users (
   id serial PRIMARY KEY,
   name VARCHAR(20) NOT NULL,
   password TEXT NOT NULL,
   UNIQUE (name)
 );
 
-CREATE TABLE events (
+CREATE TABLE events_app.events (
   id serial PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   dateAndTime VARCHAR(50) NOT NULL,
   category VARCHAR(50) NOT NULL,
   url TEXT NOT NULL,
-  description VARCHAR(100) NOT NULL,
-  location VARCHAR(100) NOT NULL,
+  description VARCHAR(8000) NOT NULL,
+  location VARCHAR(500) NOT NULL,
   UNIQUE (name)
 );
 
 
-CREATE TABLE users_events (
-  userId serial REFERENCES users (id),
-  eventId serial REFERENCES events (id)
+CREATE TABLE events_app.users_events (
+  userId serial REFERENCES events_app.users (id),
+  eventId serial REFERENCES events_app.events (id)
 );
 
-INSERT INTO users (name, password) VALUES ('julia', 'hellowpass');
-INSERT INTO users (name, password) VALUES ('jey', 'mypassword');
-INSERT INTO users (name, password) VALUES ('kevin', 'heypass');
-INSERT INTO users (name, password) VALUES ('li', 'password123');
-INSERT INTO users (name, password) VALUES ('li', 'mypassword123');
+-- INSERT INTO users (name, password) VALUES ('julia', 'hellowpass');
+-- INSERT INTO users (name, password) VALUES ('jey', 'mypassword');
+-- INSERT INTO users (name, password) VALUES ('kevin', 'heypass');
+-- INSERT INTO users (name, password) VALUES ('li', 'password123');
+-- INSERT INTO users (name, password) VALUES ('li', 'mypassword123');
 
 
 
-INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Jeys Music Show', '2017-06-28', 'music', 'http://sanfrancisco.eventful.com/events/', 'Jeys music sucks but come on by', 'San Francisco St');
+-- INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Jeys Music Show', '2017-06-28', 'music', 'http://sanfrancisco.eventful.com/events/', 'Jeys music sucks but come on by', 'San Francisco St');
 
-INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Kitten Petting Party', '2017-06-29', 'animals', 'http://sanfrancisco.eventful.com/events/', 'Come and pet all the kitties!', 'Oakland Blvd');
+-- INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Kitten Petting Party', '2017-06-29', 'animals', 'http://sanfrancisco.eventful.com/events/', 'Come and pet all the kitties!', 'Oakland Blvd');
 
-INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Lis Art Show', '2017-06-29', 'art', 'http://sanfrancisco.eventful.com/events/', 'Art by Li, must come and see!', 'Hayward St');
+-- INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Lis Art Show', '2017-06-29', 'art', 'http://sanfrancisco.eventful.com/events/', 'Art by Li, must come and see!', 'Hayward St');
 
-INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Kevins Pizza Cook Off!', '2017-07-01', 'food', 'http://sanfrancisco.eventful.com/events/', 'Dare to challenge Kevin in a pizza cook off!', 'Berkeley Blvd');
+-- INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Kevins Pizza Cook Off!', '2017-07-01', 'food', 'http://sanfrancisco.eventful.com/events/', 'Dare to challenge Kevin in a pizza cook off!', 'Berkeley Blvd');
 
 
-INSERT INTO users_events (userId, eventId) VALUES (1, 2);
-INSERT INTO users_events (userId, eventId) VALUES (1, 4);
-INSERT INTO users_events (userId, eventId) VALUES (2, 1);
-INSERT INTO users_events (userId, eventId) VALUES (3, 1);
-INSERT INTO users_events (userId, eventId) VALUES (3, 2);
-INSERT INTO users_events (userId, eventId) VALUES (3, 3);
+-- INSERT INTO users_events (userId, eventId) VALUES (1, 2);
+-- INSERT INTO users_events (userId, eventId) VALUES (1, 4);
+-- INSERT INTO users_events (userId, eventId) VALUES (2, 1);
+-- INSERT INTO users_events (userId, eventId) VALUES (3, 1);
+-- INSERT INTO users_events (userId, eventId) VALUES (3, 2);
+-- INSERT INTO users_events (userId, eventId) VALUES (3, 3);
 
 
 
