2ab07a1f66d26a9ebe59d366dc533a6251759f94
Julia Wong <juliawong05@gmail.com>
2017-07-01 19:26:53 -0700

    connect localhost with heroku db

--- server/database/index.js
+++ server/database/index.js
@@ -12,7 +12,6 @@ const connection = {
 }
 
 
-
 // var DATABASE_URL = 'postgres://qbzvgvuvdevbhj:d8d57b137756bc3b7029b3531c69e562285752ea2b1a066c1ec4e8596ad93837@ec2-23-23-222-147.compute-1.amazonaws.com:5432/dpd0sg8kgct09';
 
 // pgs.connect(process.env.DATABASE_URL, function(err, client) {
