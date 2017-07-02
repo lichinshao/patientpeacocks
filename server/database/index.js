c4621d5bf1c486ce4aab2b73174bcd9252f0cf98
Julia Wong <juliawong05@gmail.com>
2017-07-02 15:17:23 -0700

    db connect to heroku

--- server/database/index.js
+++ server/database/index.js
@@ -4,14 +4,16 @@ const pgs = require('pg');
 pgs.defaults.ssl = true;
 
 const connection = {
-  host: 'ec2-23-23-222-147.compute-1.amazonaws.com',
+  host: 'localhost',
   port: 5432,
   database: 'postgres',
   user: 'postgres',
   password: 'meow2meow'
+}
 
 const db = pgp(process.env.DATABASE_URL || connection);
 module.exports = db;
+
 
 // var DATABASE_URL = 'postgres://qbzvgvuvdevbhj:d8d57b137756bc3b7029b3531c69e562285752ea2b1a066c1ec4e8596ad93837@ec2-23-23-222-147.compute-1.amazonaws.com:5432/dpd0sg8kgct09';
 
@@ -24,7 +26,6 @@ module.exports = db;
 //       console.log(JSON.stringify(row));
 //     })
 // });
-
 
 
 
