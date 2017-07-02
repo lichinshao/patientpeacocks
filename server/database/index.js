464cb683487f14200b77f91d0de9b7ce4aafb8b3
Julia Wong <juliawong05@gmail.com>
2017-07-01 19:31:35 -0700

    heroku postgres connect test

--- server/database/index.js
+++ server/database/index.js
@@ -5,7 +5,7 @@ pgs.defaults.ssl = true;
 
 const connection = {
   host: 'ec2-23-23-222-147.compute-1.amazonaws.com',
-  port: process.env.PORT || 5432,
+  port: process.env.PORT || 3000,
   database: 'dpd0sg8kgct09',
   user: 'qbzvgvuvdevbhj',
   password: 'd8d57b137756bc3b7029b3531c69e562285752ea2b1a066c1ec4e8596ad93837'
