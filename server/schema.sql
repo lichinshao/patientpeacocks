2de6fdde6f1bc6eabbba82bbff3fc2230552c5d2
Julia Wong <juliawong05@gmail.com>
2017-07-02 18:03:50 -0700

    deployment and heroku postgres success

--- server/schema.sql
+++ server/schema.sql
@@ -45,29 +45,28 @@ CREATE TABLE users_events (
   eventId serial REFERENCES events (id)
 );
 
--- INSERT INTO users (name, password) VALUES ('julia', 'hellowpass');
--- INSERT INTO users (name, password) VALUES ('jey', 'mypassword');
--- INSERT INTO users (name, password) VALUES ('kevin', 'heypass');
--- INSERT INTO users (name, password) VALUES ('li', 'password123');
--- INSERT INTO users (name, password) VALUES ('li', 'mypassword123');
+INSERT INTO users (name, password) VALUES ('julia', 'hellowpass');
+INSERT INTO users (name, password) VALUES ('jey', 'mypassword');
+INSERT INTO users (name, password) VALUES ('kevin', 'heypass');
+INSERT INTO users (name, password) VALUES ('li', 'password123');
 
 
 
--- INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Jeys Music Show', '2017-06-28', 'music', 'http://sanfrancisco.eventful.com/events/', 'Jeys music sucks but come on by', 'San Francisco St');
+INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Jeys Music Show', '2017-06-28', 'music', 'http://sanfrancisco.eventful.com/events/', 'Jeys music sucks but come on by', 'San Francisco St');
 
--- INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Kitten Petting Party', '2017-06-29', 'animals', 'http://sanfrancisco.eventful.com/events/', 'Come and pet all the kitties!', 'Oakland Blvd');
+INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Kitten Petting Party', '2017-06-29', 'animals', 'http://sanfrancisco.eventful.com/events/', 'Come and pet all the kitties!', 'Oakland Blvd');
 
--- INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Lis Art Show', '2017-06-29', 'art', 'http://sanfrancisco.eventful.com/events/', 'Art by Li, must come and see!', 'Hayward St');
+INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Lis Art Show', '2017-06-29', 'art', 'http://sanfrancisco.eventful.com/events/', 'Art by Li, must come and see!', 'Hayward St');
 
--- INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Kevins Pizza Cook Off!', '2017-07-01', 'food', 'http://sanfrancisco.eventful.com/events/', 'Dare to challenge Kevin in a pizza cook off!', 'Berkeley Blvd');
+INSERT INTO events (name, dateAndTime, category, url, description, location) VALUES ('Kevins Pizza Cook Off!', '2017-07-01', 'food', 'http://sanfrancisco.eventful.com/events/', 'Dare to challenge Kevin in a pizza cook off!', 'Berkeley Blvd');
 
 
--- INSERT INTO users_events (userId, eventId) VALUES (1, 2);
--- INSERT INTO users_events (userId, eventId) VALUES (1, 4);
--- INSERT INTO users_events (userId, eventId) VALUES (2, 1);
--- INSERT INTO users_events (userId, eventId) VALUES (3, 1);
--- INSERT INTO users_events (userId, eventId) VALUES (3, 2);
--- INSERT INTO users_events (userId, eventId) VALUES (3, 3);
+INSERT INTO users_events (userId, eventId) VALUES (1, 2);
+INSERT INTO users_events (userId, eventId) VALUES (1, 4);
+INSERT INTO users_events (userId, eventId) VALUES (2, 1);
+INSERT INTO users_events (userId, eventId) VALUES (3, 1);
+INSERT INTO users_events (userId, eventId) VALUES (3, 2);
+INSERT INTO users_events (userId, eventId) VALUES (3, 3);
 
 
 
